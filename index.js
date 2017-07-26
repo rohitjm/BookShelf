5184ea371dab5a40b19961555343cc3d68d89696
Rohit Mathew <rohit@rabb.it>
2017-07-26 16:37:46 -0700

    Completed PART 2

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./config');
 
+// connect to the database and load models
+require('./server/models').connect(config.dbUri);
+
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
-
-//Tell the app to parse HTTP body messages
-app.use(bodyParser.urlencoded({ extended: false}));
+// tell the app to parse HTTP body messages
+app.use(bodyParser.urlencoded({ extended: false }));
 // pass the passport middleware
 app.use(passport.initialize());
 
@@ -23,11 +25,12 @@ passport.use('local-login', localLoginStrategy);
 const authCheckMiddleware = require('./server/middleware/auth-check');
 app.use('/api', authCheckMiddleware);
 
-//routes
+// routes
 const authRoutes = require('./server/routes/auth');
 const apiRoutes = require('./server/routes/api');
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
+
 
 // start the server
 app.listen(3000, () => {
